83ac755e945d66947916f17c3a40a4e2acd725e2
Chad Billman <chad@pennmanor.net>
2019-02-05 15:51:49 -0500

    Initial version for testing
    - Only supports getting assets

---
+++ snipe.go
@@ -0,0 +1,135 @@
+package snipe
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+const (
+	defaultLimit = "20"
+)
+
+type Snipe struct {
+	Client *http.Client
+	Key    string
+	Server string
+}
+
+type AssetQueryParams map[string]string
+
+func NewDefaultClient(server string, key string) (*Snipe, error) {
+	return &Snipe{Key: key, Server: server, Client: &http.Client{Transport: snipeAgentTransport{
+		apikey: key,
+		base:   http.DefaultTransport,
+	}}}, nil
+}
+
+func makeQueryFromMap(params map[string]string) string {
+	r := ""
+	first := true
+
+	for k, v := range params {
+
+		if !first {
+			r += "&"
+		} else {
+			first = false
+		}
+
+		r += fmt.Sprintf("%+v=%+v", url.QueryEscape(k), url.QueryEscape(v))
+	}
+
+	return r
+}
+
+func (s *Snipe) GetAssets(params AssetQueryParams) (*Assets, error) {
+
+	var r = new(Assets)
+
+	qp := makeQueryFromMap(params)
+
+	_, limitSet := params["limit"]
+	if !limitSet {
+		params["limit"] = defaultLimit
+		qp = makeQueryFromMap(params)
+	}
+
+	_, offsetSet := params["offset"]
+	if !offsetSet {
+		params["offset"] = "0"
+		qp = makeQueryFromMap(params)
+	}
+
+	url := fmt.Sprintf("%+v/api/v1/hardware?%+v", s.Server, qp)
+
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, err := s.Client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(r)
+	if err != nil {
+		return nil, err
+	}
+
+	// We need to iterate the api to fetch all the results
+	if !limitSet && len(r.Rows) != r.Total && !offsetSet {
+
+		for len(r.Rows) < r.Total {
+			var r2 = new(Assets)
+
+			os, _ := params["offset"]
+
+			offset, err := strconv.Atoi(os)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			limits, _ := params["limit"]
+			limit, err := strconv.Atoi(limits)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			offset += limit
+
+			params["offset"] = fmt.Sprintf("%+v", offset)
+			qp = makeQueryFromMap(params)
+
+			url := fmt.Sprintf("%+v/api/v1/hardware?%+v", s.Server, qp)
+
+			req, _ := http.NewRequest("GET", url, nil)
+			resp, err := s.Client.Do(req)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = json.NewDecoder(resp.Body).Decode(r2)
+			if err != nil {
+				return nil, err
+			}
+
+			r.Rows = append(r.Rows, r2.Rows...)
+
+		}
+
+	}
+
+	return r, nil
+
+}
+
+type snipeAgentTransport struct {
+	apikey string
+	base   http.RoundTripper
+}
+
+func (t snipeAgentTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %+v", t.apikey))
+	return t.base.RoundTrip(req)
+}
---
+++ types.go
@@ -0,0 +1,86 @@
+package snipe
+
+type Asset struct {
+
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	AssetTag string `json:"asset_tag"`
+	Serial   string `json:"serial"`
+	Model    struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"model"`
+	ModelNumber string      `json:"model_number"`
+	Eol         interface{} `json:"eol"`
+	StatusLabel struct {
+		ID         int    `json:"id"`
+		Name       string `json:"name"`
+		StatusType string `json:"status_type"`
+		StatusMeta string `json:"status_meta"`
+	} `json:"status_label"`
+	Category struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"category"`
+	Manufacturer struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"manufacturer"`
+	Supplier    interface{} `json:"supplier"`
+	Notes       interface{} `json:"notes"`
+	OrderNumber interface{} `json:"order_number"`
+	Company     interface{} `json:"company"`
+	Location    struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"location"`
+	RtdLocation interface{} `json:"rtd_location"`
+	Image       interface{} `json:"image"`
+	AssignedTo  struct {
+		ID             int         `json:"id"`
+		Username       string      `json:"username"`
+		Name           string      `json:"name"`
+		FirstName      string      `json:"first_name"`
+		LastName       interface{} `json:"last_name"`
+		EmployeeNumber interface{} `json:"employee_number"`
+		Type           string      `json:"type"`
+	} `json:"assigned_to"`
+	WarrantyMonths  interface{} `json:"warranty_months"`
+	WarrantyExpires interface{} `json:"warranty_expires"`
+	CreatedAt       struct {
+		Datetime  string `json:"datetime"`
+		Formatted string `json:"formatted"`
+	} `json:"created_at"`
+	UpdatedAt struct {
+		Datetime  string `json:"datetime"`
+		Formatted string `json:"formatted"`
+	} `json:"updated_at"`
+	LastAuditDate interface{} `json:"last_audit_date"`
+	NextAuditDate interface{} `json:"next_audit_date"`
+	DeletedAt     interface{} `json:"deleted_at"`
+	PurchaseDate  interface{} `json:"purchase_date"`
+	LastCheckout  struct {
+		Datetime  string `json:"datetime"`
+		Formatted string `json:"formatted"`
+	} `json:"last_checkout"`
+	ExpectedCheckin  interface{}   `json:"expected_checkin"`
+	PurchaseCost     interface{}   `json:"purchase_cost"`
+	CheckinCounter   int           `json:"checkin_counter"`
+	CheckoutCounter  int           `json:"checkout_counter"`
+	RequestsCounter  int           `json:"requests_counter"`
+	UserCanCheckout  bool          `json:"user_can_checkout"`
+	AvailableActions struct {
+		Checkout bool `json:"checkout"`
+		Checkin  bool `json:"checkin"`
+		Clone    bool `json:"clone"`
+		Restore  bool `json:"restore"`
+		Update   bool `json:"update"`
+		Delete   bool `json:"delete"`
+	} `json:"available_actions"`
+
+}
+
+type Assets struct {
+	Total int     `json:"total"`
+	Rows  []Asset `json:"rows"`
+}
