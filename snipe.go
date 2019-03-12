012c11cd704e01a56b046ac98061d948e5504e25
Nate Moss <nate@pennmanor.net>
2019-03-12 08:34:22 -0400

    enhance find feature

--- snipe.go
+++ snipe.go
@@ -178,6 +178,84 @@ func (s *Snipe) CheckinAsset(assetID string) (*Checkin, error) {
 	return r, nil
 }
 
+func (s *Snipe) GetUsers(params AssetQueryParams) (*Users, error) {
+
+	var r = new(Users)
+
+	qp := makeQueryFromMap(params)
+
+	_, limitSet := params["limit"]
+	if !limitSet {
+		params["limit"] = defaultLimit
+		qp = makeQueryFromMap(params)
+	}
+
+	_, offsetSet := params["offset"]
+	if !offsetSet {
+		params["offset"] = "0"
+		qp = makeQueryFromMap(params)
+	}
+
+	url := fmt.Sprintf("%+v/api/v1/users?%+v", s.Server, qp)
+
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, err := s.Client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(r)
+	if err != nil {
+		return nil, err
+	}
+
+	// We need to iterate the api to fetch all the results
+	if !limitSet && len(r.Rows) != r.Total && !offsetSet {
+
+		for len(r.Rows) < r.Total {
+			var r2 = new(Users)
+
+			os, _ := params["offset"]
+
+			offset, err := strconv.Atoi(os)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			limits, _ := params["limit"]
+			limit, err := strconv.Atoi(limits)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			offset += limit
+
+			params["offset"] = fmt.Sprintf("%+v", offset)
+			qp = makeQueryFromMap(params)
+
+			url := fmt.Sprintf("%+v/api/v1/users?%+v", s.Server, qp)
+
+			req, _ := http.NewRequest("GET", url, nil)
+			resp, err := s.Client.Do(req)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = json.NewDecoder(resp.Body).Decode(r2)
+			if err != nil {
+				return nil, err
+			}
+
+			r.Rows = append(r.Rows, r2.Rows...)
+
+		}
+
+	}
+
+	return r, nil
+
+}
+
 func (s *Snipe) GetUserByUsername(username string) (*User, error) {
 	var r = new(Users)
 
